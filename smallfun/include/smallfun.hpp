74261ae9c2b5b6782a0a1aecf70bb31d517e666d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-31 17:42:44 +0100

    put the vtable directly in the implementation

--- smallfun/include/smallfun.hpp
+++ smallfun/include/smallfun.hpp
@@ -3,100 +3,125 @@
 
 #include <type_traits>
 #include <cinttypes>
+#include <utility>
 
-namespace smallfun {
+namespace smallfun
+{
+template<class Signature, std::size_t Size = 128>
+struct function;
 
+template<class R, class...Xs, std::size_t Size>
+class alignas(std::intptr_t) function<R(Xs...), Size>
+{
+  using call_operator = R(*)(void* self, Xs...);
+  using copy_operator = void(*)(void* self, void*);
+  using dest_operator = void(*)(void* self);
 
-template<class ReturnType, class...Xs>
-struct SFConcept {
-  virtual ReturnType operator()(Xs...) = 0;
-  virtual void copy(void*)const = 0;
-  virtual ~SFConcept() {};
-};
+  call_operator vtbl_call;
+  char m_memory[Size];
+  copy_operator vtbl_copy;
+  dest_operator vtbl_dest;
+  bool m_allocated{};
 
-template<class F, class ReturnType, class...Xs>
-struct SFModel final
-  : SFConcept<ReturnType, Xs...> {
-  F f;
-
-  SFModel(F const& f)
-    : f(f)
-  {}
-
-  virtual void copy(void* memory)const {
-    new (memory) SFModel<F, ReturnType, Xs...>(f);
-  }
-
-  virtual ReturnType operator()(Xs...xs) {
-    return f(xs...);
-  }
-
-  virtual ~SFModel() {}
-};
-
-
-
-template<class Signature, unsigned size=128>
-struct SmallFun;
-
-template<class ReturnType, class...Xs, unsigned size>
-class alignas(std::intptr_t) SmallFun<ReturnType(Xs...), size> {
-  char memory[size];
-
-  bool allocated = 0;
-  using concept = SFConcept<ReturnType, Xs...>;
 public:
-  SmallFun(){}
+  function() = default;
 
-  template<class F,
-    std::enable_if_t<(sizeof(SFModel<F, ReturnType, Xs...>)<=size), bool> = 0 >
-  SmallFun(F const&f)
-    : allocated(sizeof(SFModel<F, ReturnType, Xs...>))
+  template<class F>
+  function(F f)
+    : m_allocated{true}
   {
-    static_assert(sizeof(SFModel<F, ReturnType, Xs...>) <= size);
-    new (memory) SFModel<F, ReturnType, Xs...>(f);
+    static_assert(sizeof(F) <= Size, "argument too large for SmallFun");
+    new (m_memory) F{std::forward<F>(f)};
+    vtbl_copy = [] (void* self, void* memory)
+    {
+      new (memory) F{*reinterpret_cast<F*>(self)};
+    };
+    vtbl_call = [] (void* self, Xs... xs) -> R
+    {
+      return (*reinterpret_cast<F*>(self))(xs...);
+    };
+    vtbl_dest = [] (void* self)
+    {
+      (*reinterpret_cast<F*>(self)).~F();
+    };
   }
 
-  SmallFun(SmallFun const& sf)
-    : allocated(sf.allocated) {
-    sf.copy(memory);
+  template<typename... Args>
+  function(function<Args...>&& sf)
+  {
+    static_assert(std::is_same<decltype(sf), void>::value, "");
   }
 
+  function(function&& sf)
+    : vtbl_call(sf.vtbl_call)
+    , vtbl_copy(sf.vtbl_copy)
+    , vtbl_dest(sf.vtbl_dest)
+    , m_allocated(sf.m_allocated)
+  {
+    sf.copy(m_memory);
+  }
 
-  SmallFun& operator=(SmallFun const& sf) {
+  function(const function& sf)
+    : vtbl_call(sf.vtbl_call)
+    , vtbl_copy(sf.vtbl_copy)
+    , vtbl_dest(sf.vtbl_dest)
+    , m_allocated(sf.m_allocated)
+  {
+    sf.copy(m_memory);
+  }
+
+  function& operator=(const function& sf)
+  {
     clean();
-    allocated = sf.allocated;
-    sf.copy(memory);
+    vtbl_call = sf.vtbl_call;
+    vtbl_copy = sf.vtbl_copy;
+    vtbl_dest = sf.vtbl_dest;
+    sf.copy(m_memory);
+    m_allocated = sf.m_allocated;
     return *this;
   }
 
-  void clean() {
-    if (allocated) {
-      ((concept*)memory)->~concept();
-      allocated = 0;
-    }
-  }
-
-  ~SmallFun() {
-    if (allocated) {
-      ((concept*)memory)->~concept();
+  ~function()
+  {
+    if (m_allocated)
+    {
+      destruct();
     }
   }
 
   template<class...Ys>
-  ReturnType operator()(Ys&&...ys) {
-    return (*(concept*)memory)(std::forward<Ys>(ys)...);
+  R operator()(Ys&&...ys)
+  {
+    return vtbl_call((void*)m_memory, std::forward<Ys>(ys)...);
   }
 
   template<class...Ys>
-  ReturnType operator()(Ys&&...ys) const {
-    return (*(concept*)memory)(std::forward<Ys>(ys)...);
+  R operator()(Ys&&...ys) const
+  {
+    return vtbl_call((void*)m_memory, std::forward<Ys>(ys)...);
   }
 
-  void copy(void* data)const {
-    if (allocated) {
-      ((concept*)memory)->copy(data);
+private:
+  void clean()
+  {
+    if (m_allocated)
+    {
+      destruct();
+      m_allocated = false;
     }
+  }
+
+  void copy(void* data) const
+  {
+    if (m_allocated)
+    {
+      return vtbl_copy((void*)m_memory, data);
+    }
+  }
+
+  void destruct()
+  {
+    return vtbl_dest((void*)m_memory);
   }
 };
 
