0c9380892d192d153e05e27528bc6bcc874da4bc
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-01-07 18:05:46 +0100

    emscripten seemingly requires double alignment by default

--- smallfun/include/smallfun.hpp
+++ smallfun/include/smallfun.hpp
@@ -7,7 +7,11 @@
 
 namespace smallfun
 {
+#if defined(__EMSCRIPTEN__)
+template<class Signature, std::size_t Size = 128, std::size_t Align = 2 * sizeof(std::intptr_t)>
+#else
 template<class Signature, std::size_t Size = 128, std::size_t Align = sizeof(std::intptr_t)>
+#endif
 struct function;
 
 template<class R, class...Xs, std::size_t Size, std::size_t Align>
